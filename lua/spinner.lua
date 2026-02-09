a28f30e9f975f555c336b6d8b39429c526e738c8
NorinB <accounts@noahbauer.de>
2026-02-09 12:35:00 +0100

    [REFAC] spinner: use notifications as backend

--- lua/spinner.lua
+++ lua/spinner.lua
@@ -17,12 +17,12 @@ local config = {
   show_spinner_regexes = {
     "projects",
   },
+  notification_id = "lsp_spinner",
 }
 
 local spinner_index = 1
 local spinner_timer = nil
-local spinner_buf = nil
-local spinner_win = nil
+local spinner_id = nil
 
 M.should_show_spinner = function()
   for _, pattern in ipairs(config.show_spinner_regexes) do
@@ -38,31 +38,20 @@ end
 function M.show(msg, title)
   M.hide()
   msg = msg ~= nil and msg or ""
-  -- Default position: the top right corner
-  local win_options = {
-    relative = "editor",
-    width = #msg + 7,
-    height = 1,
-    col = vim.o.columns - 1,
-    row = vim.o.lines - 5,
-    style = "minimal",
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-    title = title,
-  }
-
-  spinner_buf = vim.api.nvim_create_buf(false, true)
-  spinner_win = vim.api.nvim_open_win(spinner_buf, false, win_options)
 
   spinner_timer = vim.loop.new_timer()
   spinner_timer:start(
     0,
     100,
     vim.schedule_wrap(function()
-      if vim.api.nvim_buf_is_valid(spinner_buf) then
-        vim.api.nvim_buf_set_lines(spinner_buf, 0, -1, false, {
-          (#msg > 0 and " " .. msg .. "    " or "") .. config.spinner_frames[spinner_index] .. (#msg > 0 and " " or ""),
-        })
-      end
+      vim.notify((#msg > 0 and " " .. msg .. "    " or "") .. (#msg > 0 and " " or ""), vim.log.levels.INFO, {
+        id = config.notification_id,
+        title = "LSP",
+        opts = function(notification)
+          notification.icon = config.spinner_frames[spinner_index]
+          spinner_id = notification.id
+        end,
+      })
       spinner_index = spinner_index % #config.spinner_frames + 1
     end)
   )
@@ -75,11 +64,10 @@ function M.hide(msg)
     spinner_timer:stop()
     spinner_timer:close()
     spinner_timer = nil
-    if spinner_win then
-      vim.api.nvim_win_close(spinner_win, true)
-    end
-    if spinner_buf then
-      vim.api.nvim_buf_delete(spinner_buf, { force = true })
+
+    if spinner_id ~= nil then
+      Snacks.notifier.hide(config.notification_id)
+      spinner_id = nil
     end
 
     if msg ~= nil then
